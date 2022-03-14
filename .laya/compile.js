0fed7c2cab67ab2be898f225b112ed201339aa9c
darklinden <darklinden@gmail.com>
2022-03-14 10:41:58 +0800

    change useSourceMap to true to enable debug

--- .laya/compile.js
+++ .laya/compile.js
@@ -3,9 +3,14 @@
 const useIDENode = process.argv[0].indexOf("LayaAir") > -1 ? true : false;
 const useCMDNode = process.argv[1].indexOf("layaair2-cmd") > -1 ? true : false;
 
-function useOtherNode(){
-	return useIDENode||useCMDNode;
+function useOtherNode() {
+	return useIDENode || useCMDNode;
 }
+
+function useSourceMap() {
+	return true;
+}
+
 //获取Node插件和工作路径
 let ideModuleDir = useOtherNode() ? process.argv[1].replace("gulp\\bin\\gulp.js", "").replace("gulp/bin/gulp.js", "") : "";
 let workSpaceDir = useOtherNode() ? process.argv[2].replace("--gulpfile=", "").replace("\\.laya\\compile.js", "").replace("/.laya/compile.js", "") : "./../";
@@ -25,18 +30,18 @@ if (global.publish) {
 
 gulp.task("compile", prevTasks, function () {
 	// 发布时调用编译功能，判断是否点击了编译选项
-    if (global.publish) {
-        workSpaceDir = global.workSpaceDir; // 发布时调用编译，workSpaceDir使用publish.js里的变量
-        let forceCompile = !fs.existsSync(path.join(workSpaceDir, "bin", "js", "bundle.js")); // 发布时，并且没有编译过，则强制编译
-        if (!global.config.compile && !forceCompile) {
-            return;
-        }
-    }
+	if (global.publish) {
+		workSpaceDir = global.workSpaceDir; // 发布时调用编译，workSpaceDir使用publish.js里的变量
+		let forceCompile = !fs.existsSync(path.join(workSpaceDir, "bin", "js", "bundle.js")); // 发布时，并且没有编译过，则强制编译
+		if (!global.config.compile && !forceCompile) {
+			return;
+		}
+	}
 
 	return rollup.rollup({
 		input: workSpaceDir + '/src/Main.ts',
-		onwarn:(waring,warn)=>{
-			if(waring.code == "CIRCULAR_DEPENDENCY"){
+		onwarn: (waring, warn) => {
+			if (waring.code == "CIRCULAR_DEPENDENCY") {
 				console.log("warnning Circular dependency:");
 				console.log(waring);
 			}
@@ -44,32 +49,32 @@ gulp.task("compile", prevTasks, function () {
 		treeshake: false, //建议忽略
 		plugins: [
 			typescript({
-				tsconfig:workSpaceDir + "/tsconfig.json",
+				tsconfig: workSpaceDir + "/tsconfig.json",
 				check: true, //Set to false to avoid doing any diagnostic checks on the code
-				tsconfigOverride:{compilerOptions:{removeComments: true}},
-				include:/.*.ts/,
+				tsconfigOverride: { compilerOptions: { removeComments: true } },
+				include: /.*.ts/,
 			}),
 			glsl({
 				// By default, everything gets included
 				include: /.*(.glsl|.vs|.fs)$/,
-				sourceMap: false,
-				compress:false
+				sourceMap: useSourceMap(),
+				compress: false
 			}),
 			/*terser({
 				output: {
 				},
 				numWorkers:1,//Amount of workers to spawn. Defaults to the number of CPUs minus 1
-				sourcemap: false
-			})*/        
+				sourcemap: useSourceMap()
+			})*/
 		]
 	}).then(bundle => {
 		return bundle.write({
 			file: workSpaceDir + '/bin/js/bundle.js',
 			format: 'iife',
 			name: 'laya',
-			sourcemap: false
+			sourcemap: useSourceMap()
 		});
-	}).catch(err=>{
+	}).catch(err => {
 		console.log(err);
 	});
 });
